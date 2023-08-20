9ce94d11bec1000fa34312a8d4665fcb738974c1
amoghp <amoghp@testvagrant.com>
2023-08-20 20:39:55 +0530

    added driverManager and updated ClientSignInTests

--- src/test/java/org/example/ClientSignInTests.java
+++ src/test/java/org/example/ClientSignInTests.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import io.appium.java_client.AppiumDriver;
+import org.example.drivers.AndroidDriverManager;
+import org.example.drivers.iOSManager;
 import org.example.models.User;
 import org.example.screens.HomeScreen;
 import org.example.screens.LauncherScreen;
 import org.example.screens.ProfileScreen;
 import org.example.screens.SignInScreen;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,17 +18,25 @@ public class ClientSignInTests{
     public void ClientSignInTests() {
 
         //Arrange
-        AppiumDriver appiumDriver=null;
-        LauncherScreen launcherScreen=new LauncherScreen(appiumDriver);
+        AppiumDriver driver1=null;
+        DesiredCapabilities caps=new DesiredCapabilities();
+        caps.setCapability("platformName","android");
+        caps.setCapability("deviceName","Pixel 6");
+        caps.setCapability("automationName","UiAutomator2");
+        caps.setCapability("app","");
+        AppiumDriver driver = new AndroidDriverManager().create(caps);   //created an AndroidManager obj and called create method which takes capabilities obj as parameter
+        AppiumDriver iOSDriver = new iOSManager().create(caps);
+
+        LauncherScreen launcherScreen=new LauncherScreen(driver);
         launcherScreen.navToClientSignIn();
         User client = new User("randomclient@gmail.com",
                 "password", "Automation Client",
                 "1234567890","02/2/2023");
 
         //Act
-        SignInScreen signInScreen=new SignInScreen(appiumDriver);
+        SignInScreen signInScreen=new SignInScreen(driver);
         HomeScreen homeScreen = signInScreen.signIn(client);
-        ProfileScreen profileScreen = new ProfileScreen(appiumDriver);
+        ProfileScreen profileScreen = new ProfileScreen(driver);
         User clientProfile = profileScreen.getProfileDetails();
 
         //Assert
